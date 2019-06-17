7df6f99b242040ad6c544d955b871d325a226b9d
dennis guye <dennis@aion.network>
2019-06-17 10:07:17 -0400

    changed query parameter to to_addr

--- aion-api/src/main/java/com/aion/dashboard/services/TransactionService.java
+++ aion-api/src/main/java/com/aion/dashboard/services/TransactionService.java
@@ -87,7 +87,7 @@ public class TransactionService {
 				if(!txn.getTransactionLog().equals("[]")) {
 					List<TokenTransfers> tknTxfsList = tknTxfRepo.findByTransactionHash(txn.getTransactionHash());
 					if (!tknTxfsList.isEmpty()) {
-						Token tkn = tknRepo.findByContractAddr(txn.getContractAddr());
+						Token tkn = tknRepo.findByContractAddr(txn.getToAddr());
 						JSONArray transfersArray = new JSONArray();
 						for (TokenTransfers tknTxf : tknTxfsList) {
 							JSONObject object = new JSONObject();
