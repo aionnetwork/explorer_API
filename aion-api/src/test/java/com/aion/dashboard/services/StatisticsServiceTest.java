22aa3e420e9eb6cc9342d52c739384454f831b54
dennis guye <dennis@aion.network>
2019-07-25 12:56:06 -0400

    Completed merge

--- aion-api/src/test/java/com/aion/dashboard/services/StatisticsServiceTest.java
+++ aion-api/src/test/java/com/aion/dashboard/services/StatisticsServiceTest.java
@@ -11,8 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = AionDashboardApiApplication.class)
@@ -38,5 +39,29 @@ class StatisticsServiceTest {
 
         assertNotNull(metricsDTOResult);
         assertFalse(metricsDTOResult.getContent().isEmpty());
+    }
+
+    @org.junit.Test
+    public void createHealthDTO(){
+        var res0 = StatisticsService.createHealthFrom(1,1, System.currentTimeMillis()/1000);
+        var res1 = StatisticsService.createHealthFrom(1,6, System.currentTimeMillis()/1000);
+        var res2 = StatisticsService.createHealthFrom(1,1, System.currentTimeMillis()/1000 - 10*60);
+
+        assertEquals("OK", res0.getStatus());
+        assertEquals("SYNCING", res1.getStatus());
+        assertEquals("STALLED", res2.getStatus());
+
+
+
+        assertEquals(res0.getDbBlockHead(), 1);
+        assertEquals(res0.getBlockchainHead(), 1);
+        assertEquals(res1.getDbBlockHead(), 1);
+        assertEquals(res1.getBlockchainHead(), 6);
+        assertEquals(res2.getDbBlockHead(), 1);
+        assertEquals(res2.getBlockchainHead(),1);
+
+        var currStatus = assertDoesNotThrow(()-> service.health());
+        assertNotNull(currStatus);
+        assertTrue(List.of("OK", "STALLED", "SYNCING").contains(currStatus.getStatus()));
     }
 }
