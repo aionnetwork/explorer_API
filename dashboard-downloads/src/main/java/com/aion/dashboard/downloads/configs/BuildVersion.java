d2db5725723e7647aa4ef70466f344c5b9d73e4b
Jesus Redon <jesus@aion.network>
2019-10-02 11:50:11 -0400

    Added env for database connection

--- dashboard-downloads/src/main/java/com/aion/dashboard/downloads/configs/BuildVersion.java
+++ dashboard-downloads/src/main/java/com/aion/dashboard/downloads/configs/BuildVersion.java
@@ -7,10 +7,10 @@ package com.aion.dashboard.downloads.configs;
 public class BuildVersion {
 	public static final String MAVEN_GROUP = "aion-exporter";
 	public static final String MAVEN_NAME = "aion-exporter";
-	public static final String VERSION = "2.0-0834bb6-0353-19-06-2019";
-	public static final int GIT_REVISION = 42;
-	public static final String GIT_SHA = "0834bb69de589e39a43a19d1d894873a2a512298";
-	public static final String GIT_DATE = "2019-06-19T15:33:15Z";
-	public static final String BUILD_DATE = "2019-06-19T15:53:44Z";
-	public static final long BUILD_UNIX_TIME = 1560959624808L;
+	public static final String VERSION = "2.0-2f3a157-0406-02-10-2019";
+	public static final int GIT_REVISION = 56;
+	public static final String GIT_SHA = "2f3a15719900336cf0e4496d1a9986a7a4ad7599";
+	public static final String GIT_DATE = "2019-10-02T15:50:11Z";
+	public static final String BUILD_DATE = "2019-10-02T16:06:31Z";
+	public static final long BUILD_UNIX_TIME = 1570032391703L;
 }
